a77f8b20eae1deee7c5040d4afa4490f496ee350
DAXILSAXENA <daxildaxil@gmail.com>
2021-06-21 11:38:58 +0530

    Added Player check for a option,they are no play,ladder or snake

--- SnakeAndLadderProblem.java
+++ SnakeAndLadderProblem.java
@@ -1,6 +1,31 @@
 public class SnakeAndLadderProblem {
-    public static void main(String[] args) {
-        int roll_die = (int)((Math.floor(Math.random() * 10)) % 6) + 1;
-        System.out.println("Output of rolling the die is: " +roll_die);
-    }
+	public static final int noPlay = 0;
+	public static final int ladder = 1;
+	public static final int snake = 2;
+
+	//User check for a option
+	public static void checkOption() {
+		int initial_position = 0;
+		int roll_die = (int) ((Math.floor(Math.random() * 10)) % 6) + 1;
+		int check_option = (int) ((Math.floor(Math.random() * 10)) % 3);
+		switch (check_option) {
+		case noPlay:
+			initial_position += 0;
+			System.out.println(initial_position);
+			break;
+		case ladder:
+			initial_position += roll_die;
+			System.out.println(initial_position);
+			break;
+		case snake:
+			initial_position -= roll_die;
+			System.out.println(initial_position);
+			break;
+		}
+
+	}
+
+	public static void main(String[] args) {
+		checkOption();
+	}
 }
